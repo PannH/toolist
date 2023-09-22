cd7930421ec800bf6f92b05867dc85182b988fcc
PannH <pannh2609@gmail.com>
2023-09-22 12:40:38 +0200

    create CounterTitle component

---
+++ src/components/ui/CounterTitle.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+   const { count } = defineProps<{
+      count: number;
+   }>();
+</script>
+
+<template>
+   <h1 class="counter-title">
+      <slot />
+      <div class="count">{{ count }}</div>
+   </h1>
+</template>
+
+<style scoped lang="scss">
+   @use '../../styles/vars.scss' as *;
+
+   .counter-title {
+      color: $white;
+      font-weight: 500;
+
+      display: flex;
+
+      > .count {
+         color: $gray;
+         font-size: 18px;
+
+         padding-inline: 5px;
+      }
+   }
+</style>
