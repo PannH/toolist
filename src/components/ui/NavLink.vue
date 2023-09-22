0c65e8476140d6b710374e4a8923a1eace7c2d43
PannH <pannh2609@gmail.com>
2023-09-22 11:30:19 +0200

    create NavLink component

---
+++ src/components/ui/NavLink.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+   import { RouterLink } from 'vue-router';
+
+   const { to } = defineProps<{
+      to: string;
+   }>();
+</script>
+
+<template>
+   <RouterLink class="nav-link" :to="to">
+      <slot />
+   </RouterLink>
+</template>
+
+<style scoped lang="scss">
+   @use '../../styles/vars.scss'  as *;
+
+   .nav-link {
+      padding: 5px 20px;
+
+      border-radius: 50px;
+
+      text-decoration: none;
+
+      transition: background-color .1s;
+
+      font-size: 16px;
+
+      &:hover {
+         background-color: $transparent-white;
+      }
+   }
+</style>
