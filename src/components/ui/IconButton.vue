01e8e15f94bc0bdbdd15b731bdaa103df9cb3f76
PannH <pannh2609@gmail.com>
2023-09-22 18:35:52 +0200

    create the IconButton component

---
+++ src/components/ui/IconButton.vue
@@ -0,0 +1,38 @@
+<template>
+   <button class="icon-button">
+      <slot />
+   </button>
+</template>
+
+<style scoped lang="scss">
+   @use '../../styles/vars.scss'  as *;
+
+   .icon-button {
+      background: none;
+
+      border: none;
+      border-radius: 50%;
+
+      display: grid;
+      place-items: center;
+
+      color: $white;
+
+      padding: 12px;
+
+      transition: background-color .1s;
+
+      &:hover {
+         cursor: pointer;
+      
+         background-color: $transparent-gray;
+      }
+
+      > svg {
+         $width: 24px;
+
+         width: $width;
+         height: $width;
+      }
+   }
+</style>
